3d85493fc2268b6cdc3c4b8d0fa78e512eda9d23
Mason <sorom2@student.op.ac.nz>
2020-03-27 18:56:09 +1300

    Make led matrix work, add fonts and global mode enum

--- thermometer/thermometer.ino
+++ thermometer/thermometer.ino
@@ -12,12 +12,22 @@ enum mode {
 };
 
 #define TEMP_PIN 0
+#define DELAY_MS 2000
+
+const uint8_t* FONT_1ROW = Arial14;
+const uint8_t* FONT_2ROW = SystemFont5x7;
+enum mode current_mode;
+
+SoftDMD dmd(WIDTH, HEIGHT);
 
 void setup() {
-
+   dmd.setBrightness(255);
+   dmd.begin();
+   dmd.selectFont(FONT_1ROW);
 }
 
 void loop() {
-
+    dmd.drawString(0,0, "Hello, World!");
+    delay(DELAY_MS);
 }
 
