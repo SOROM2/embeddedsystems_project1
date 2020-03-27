33bcaab5ac3979fe6807a88a281aef8263f206fa
Mason <sorom2@student.op.ac.nz>
2020-03-27 18:50:00 +1300

    init commit

---
+++ thermometer/thermometer.ino
@@ -0,0 +1,17 @@
+#include <DMD2.h>
+#include <fonts/Arial14.h>
+#include <fonts/SystemFont5x7.h>
+
+#define WIDTH 1
+#define HEIGHT 1
+
+#define TEMP_PIN 0
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
